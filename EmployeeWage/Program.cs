376bdda16600a5fd4b40c3ce96cefcd5431ed78a
Sai Kumar Naik <saikumar.naik7777@gmail.com>
2020-09-28 17:26:21 +0530

    [Sai]Refactor Calculate Daily Wage

--- EmployeeWage/Program.cs
+++ EmployeeWage/Program.cs
@@ -8,23 +8,32 @@ namespace EmployeeWage
     {
         static void Main(string[] args)
         {
-            
-                Random rand = new Random();
 
-                int is_attended = rand.Next(0, 2);
+            Random rand = new Random();
 
-                int attended = 1;
+            //constants
+            int IS_FULL_TIME = 1;
+            int EMP_RATE_PER_HOUR = 20;
 
+            //variables
 
-                if (is_attended == attended)
-                {
-                    Console.Out.WriteLine("Employee is present");
-                }
-                else
-                {
-                    Console.Out.WriteLine("Employee is absent");
-                }
-           
+            int empWage = 0;
+            int empHours = 0;
+
+            int empCheck = rand.Next(0, 2);
+
+            if (IS_FULL_TIME == empCheck)
+            {
+                empHours = 8;
+            }
+            else
+            {
+                empHours = 0;
+            }
+
+            empWage = empHours * EMP_RATE_PER_HOUR;
+
+            Console.Out.WriteLine("Wage is : " + empWage);
         }
     }
 }
