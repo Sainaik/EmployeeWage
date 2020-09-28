fef521716d1ec65bc3e3021720a86436f7aaac2a
Sai Kumar Naik <saikumar.naik7777@gmail.com>
2020-09-28 20:42:03 +0530

    [Sai]Refactor Added class method(static) to calc wage

--- EmployeeWage/Program.cs
+++ EmployeeWage/Program.cs
@@ -4,26 +4,27 @@ namespace EmployeeWage
 {
     class Program
     {
-        static void Main(string[] args)
+        //constants
+
+        const int IS_FULL_TIME = 1;
+        const int IS_PART_TIME = 2;
+        const int TOTAL_WORKING_HOURS = 100;
+        const int TOTAL_WORKING_DAYS = 20;
+        const int WAGE_PER_HOUR = 20;
+
+
+        public static int CalculateWage()
         {
-            Random rand = new Random();
-
-            //constants
-
-            const int IS_FULL_TIME = 1;
-            const int IS_PART_TIME = 2;
-            const int TOTAL_WORKING_HOURS = 100;
-            const int TOTAL_WORKING_DAYS = 20;
-            const int WAGE_PER_HOUR = 20;
-
             //variable
 
             int numberOfHours = 0;
             int workingDays = 0;
             int total_wage = 0;
 
+
             while (numberOfHours <= TOTAL_WORKING_HOURS && workingDays <= TOTAL_WORKING_DAYS)
             {
+                Random rand = new Random();
                 int type = rand.Next(0, 3);
                 int hours = 0;
                 switch (type)
@@ -41,7 +42,7 @@ namespace EmployeeWage
                 }
 
                 numberOfHours += hours;
-               
+
                 Console.Out.WriteLine("Day " + workingDays + " hours " + hours);
 
                 workingDays++;
@@ -49,6 +50,12 @@ namespace EmployeeWage
 
             total_wage = numberOfHours * WAGE_PER_HOUR;
 
+            return total_wage;
+        }
+
+        static void Main(string[] args)
+        {
+            int total_wage = CalculateWage();
             Console.Out.WriteLine("Total wage of the month :" + total_wage);
 
         }
