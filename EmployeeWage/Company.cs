ee12ea3e628bf1192c8c650856de8de8c7dfe395
Sai Kumar Naik <saikumar.naik7777@gmail.com>
2020-09-28 21:52:29 +0530

    [Sai]Refactor. Saving wages of companies

---
+++ EmployeeWage/Company.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmployeeWage
+{
+    class Company
+    {
+        public String comp_name;
+        public int wage_per_hour;
+        public int total_working_days;
+        public int total_working_hours;
+
+
+        public Company(String comp_name, int wage_per_hour, int total_working_days, int total_working_hours)
+        {
+            this.comp_name = comp_name;
+            this.wage_per_hour = wage_per_hour;
+            this.total_working_days = total_working_days;
+            this.total_working_hours = total_working_hours;
+
+        }
+    }
+}
