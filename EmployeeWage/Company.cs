50f3468d54a90b2488f1f7814a12fbe094973e38
Sai Kumar Naik <saikumar.naik7777@gmail.com>
2020-10-02 10:58:06 +0530

    Reverted. Added a EmpWageBuilder for each company

--- EmployeeWage/Company.cs
+++ EmployeeWage/Company.cs
@@ -6,18 +6,24 @@ namespace EmployeeWage
 {
     class Company
     {
-        public String comp_name;
-        public int wage_per_hour;
-        public int total_working_days;
-        public int total_working_hours;
+        //getters and setters for the instance variables
+
+        public String CompName { get; set; }
+        public int WagePerHour { get; set; }
+        public int TotalWorkingDays { get; set; }
+        public int TotalWorkingHours { get; set; }
+
+        public int MonthlyTotalWage { get; set; }
 
 
-        public Company(String comp_name, int wage_per_hour, int total_working_days, int total_working_hours)
+        //company constructor
+        public Company(String compName, int wagePerHour, int totalWorkingDays, int totalWorkingHours)
         {
-            this.comp_name = comp_name;
-            this.wage_per_hour = wage_per_hour;
-            this.total_working_days = total_working_days;
-            this.total_working_hours = total_working_hours;
+            this.CompName = compName;
+            this.WagePerHour = wagePerHour;
+            this.TotalWorkingDays = totalWorkingDays;
+            this.TotalWorkingHours = totalWorkingHours;
+            this.MonthlyTotalWage = 0;
 
         }
     }
--- EmployeeWage/Program.cs
+++ EmployeeWage/Program.cs
@@ -6,85 +6,28 @@ namespace Employee_Wage_calc_Problem
 {
     class Program
     {
-        //constants
-
-        const int IS_FULL_TIME = 1;
-        const int IS_PART_TIME = 2;
 
 
-        public static int CalculateWage(String comp_name, int wage_per_hour, int total_working_days, int total_working_hours)
-        {
-            Random rand = new Random();
 
-            //variables
-
-            int numberOfHours = 0;
-            int workingDays = 1;
-            int totalWage = 0;
-
-
-            // run till the number of hours or workingdays reached in respective company
-
-            while (numberOfHours <= total_working_hours && workingDays <= total_working_days)
-            {
-
-                int type = rand.Next(0, 3);
-                int hours = 0;
-
-                // switch to type of Employee
-
-                switch (type)
-                {
-                    case IS_FULL_TIME:
-                        hours = 8;
-                        break;
-                    case IS_PART_TIME:
-                        hours = 4;
-                        break;
-                    default:
-                        hours = 0;
-                        break;
-
-                }
-
-                // add the hours to total hrs
-
-                numberOfHours += hours;
-
-                Console.Out.WriteLine(comp_name + " Day " + workingDays + " hours " + hours);
-
-                workingDays++;
-            }
-
-            totalWage = numberOfHours * wage_per_hour;
-
-            return totalWage;
-        }
         static void Main(string[] args)
         {
+             //public Dictionary<String, double> totalWages = new Dictionary<String, double>();
 
-            int total_wage = 0;
+          
+            EmpWageBuilder ew1 = new EmpWageBuilder("Apple", 150, 20, 100);
 
-            Dictionary<Company, int> companies = new Dictionary<Company, int>();
+            ew1.CalculateWage();
 
-            Company apple = new Company("Apple", 150, 20, 100);
+            Console.Out.WriteLine("\n**************************************\n");
 
-            total_wage = CalculateWage(apple.comp_name, apple.wage_per_hour, apple.total_working_days, apple.total_working_hours);
+            EmpWageBuilder ew2 = new EmpWageBuilder("Google", 125, 22, 125);
 
-            companies.Add(apple, total_wage);
-
-            Company google = new Company("Google", 125, 22, 125);
-
-           
-            total_wage = CalculateWage(google.comp_name, google.wage_per_hour, google.total_working_days, google.total_working_hours);
-
-            companies.Add(google, total_wage);
-
-            foreach (KeyValuePair<Company, int> comp in companies)
-            {
-                Console.WriteLine(comp.Key.comp_name + "'s Wage : " + comp.Value);
-            }
-
+            ew2.CalculateWage();
         }
-    }
+            
+
+     }
+
+        
 }
+
